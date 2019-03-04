02e5b5bd840bd97540580e2b988a8de595546e27
Joel Chen <xchen@walmartlabs.com>
2019-03-03 21:00:57 -0800

    proper test verifications

--- test/spec/index.spec.js
+++ test/spec/index.spec.js
@@ -3,6 +3,7 @@
 const Munchy = require("../..");
 const fs = require("fs");
 const { PassThrough } = require("stream");
+const { asyncVerify, expectError } = require("run-verify");
 
 describe("munchy", function() {
   const drainIt = munchy => {
@@ -30,18 +31,14 @@ describe("munchy", function() {
       end = true;
     });
 
-    return new Promise((resolve, reject) => {
-      munchy.on("close", () => {
-        try {
-          expect(end, "didn't get end event before close event").to.be.true;
-          const output = data.map(x => x.toString());
-          expect(output).to.deep.equal(["hello world\n", "foo\n", "blah", "bar\n"]);
-          resolve();
-        } catch (err) {
-          reject(err);
-        }
-      });
-    });
+    return asyncVerify(
+      next => munchy.on("close", next),
+      () => {
+        expect(end, "didn't get end event before close event").to.be.true;
+        const output = data.map(x => x.toString());
+        expect(output).to.deep.equal(["hello world\n", "foo\n", "blah", "bar\n"]);
+      }
+    );
   });
 
   it("should initialize sources to []", () => {
@@ -56,51 +53,43 @@ describe("munchy", function() {
 
     const { data } = drainIt(munchy);
 
-    return new Promise((resolve, reject) => {
-      foo.on("end", () => {
-        process.nextTick(() => {
-          munchy.munch(fs.createReadStream("test/fixtures/foo.txt"), bar);
-          bar.on("end", () => {
-            munchy.munch(null);
-            munchy.on("end", () => {
-              try {
-                expect(data.map(x => x.toString().trim()).join("")).to.equal("helloworldfoofoobar");
-                resolve();
-              } catch (err) {
-                reject(err);
-              }
-            });
-          });
-        });
-      });
-    });
+    return asyncVerify(
+      next => foo.on("end", next),
+      () => munchy.munch(fs.createReadStream("test/fixtures/foo.txt"), bar),
+      next => bar.on("end", next),
+      () => munchy.munch(null),
+      next => munchy.on("end", next),
+      () => {
+        expect(data.map(x => x.toString().trim()).join("")).to.equal("helloworldfoofoobar");
+      }
+    );
   });
 
   it("should throw if trying to read after destroy", () => {
     const munchy = new Munchy({}, "hello", "world", null);
     drainIt(munchy);
-    return new Promise((resolve, reject) => {
-      munchy.on("end", () => {
-        try {
-          expect(() => munchy.munch("test")).to.throw("called after destroy");
-          resolve();
-        } catch (err) {
-          reject(err);
-        }
-      });
-    });
+    return asyncVerify(
+      next => munchy.on("end", next),
+      () => {
+        expect(() => munchy.munch("test")).to.throw("called after destroy");
+      }
+    );
   });
 
-  it("should error if a source stream error", done => {
+  it("should error if a source stream error", () => {
     const munchy = new Munchy();
 
     const p = new PassThrough();
     drainIt(munchy);
     munchy.munch(p);
-    munchy.on("error", err => {
-      expect(err.message).to.equal("test");
-      done();
-    });
-    p.emit("error", new Error("test"));
+    return asyncVerify(
+      expectError(next => {
+        munchy.on("error", next);
+        p.emit("error", new Error("test"));
+      }),
+      err => {
+        expect(err.message).to.equal("test");
+      }
+    );
   });
 });
